fbcaabcb68583217a04a9141f01ee5a3b933d420
serfusE <serfuse@gmail.com>
2019-05-15 10:35:55 +0800

    add unit tests.

--- letsmeet.py
+++ letsmeet.py
@@ -6,6 +6,15 @@ from flask_migrate import Migrate
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
+
 @app.shell_context_processor
 def make_shell_context():
 	return dict(db=db, User=User, Event=Event, User_Event=User_Event)
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
